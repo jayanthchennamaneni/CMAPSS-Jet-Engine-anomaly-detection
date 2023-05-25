5acbd590f63a26aba3ad87127192be864bf4be5f
CJ <jayanthrao1795@gmail.com>
2023-05-25 23:02:03 +0200

    dropped NaN columns

--- eda.py
+++ eda.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
 # Specify the file path
-file_path = r'/Users/cj/Desktop/CMAPSS-Jet-Engine-anomaly-detection/data/train_FD001.txt'
+file_path = r'/Users/cj/Desktop/CMAPSS-Jet-Engine-anomaly-detection/data/train_FD004.txt'
 
 # Read the text file into a pandas DataFrame
 df = pd.read_csv(file_path, delimiter=' ', header=None)
 
+# Drop columns with NaN values
+df = df.dropna(axis=1, how='all')
 
 # Check for missing values
-print(df.isnull().sum())
+df.head(1)
+
 
 
 
